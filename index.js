1dd5c519e99ea7968025e51fb72b0835d1f0bf13
naitoon819 <hayate4323@gmail.com>
2022-06-08 09:52:06 +0900

    kousin2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 for (let n = 1; n <= 16; n = n + 1) {
-const piece01 = document.querySelector('.pos-1');
-piece01.style.order = parseInt(Math.random() * 16) + 1;
+  const piece01 = document.querySelector('.pos-1');
+
+  //memo:1~16ランダムに数値を決めると、番号が被る場合がある
+  piece01.style.order = parseInt(Math.random() * 16) + 1;
 
 }
